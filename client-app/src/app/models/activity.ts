87d8725c231982dcbf9f690530647121e55ab249
fjolla ismaili <fjolla.ismaili.fi@gmail.com>
2021-11-08 18:42:11 +0100

    End of section 5

---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,10 @@
+export interface Activity {
+    id: string;
+    title: string;
+    description: string;
+    date: string;
+     category: string;
+    city: string;
+    venue: string;
+   
+}
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/style.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 
